f47a79933007e6b016c70cac747a4c6d50ce8d7a
FacuKargman <facukargman@gmail.com>
2025-06-26 17:49:37 -0300

    strategy+

--- SistemaEncuentrosJava/src/state/Confirmado.java
+++ SistemaEncuentrosJava/src/state/Confirmado.java
@@ -4,8 +4,27 @@ import entities.Encuentro;
 
 public class Confirmado implements EstadoPartido {
     private Encuentro encuentro;
-    public Confirmado(Encuentro e) { this.encuentro = e; }
-    @Override public void manejarCambioEstado() {}
-    @Override public boolean puedeUnirse() { return false; }
-    @Override public boolean puedeConfirmar() { return false; }
+
+    public Confirmado(Encuentro encuentro) {
+        this.encuentro = encuentro;
+    }
+
+    @Override
+    public void manejarCambioEstado() {
+        System.out.println("Estado cambiado: Confirmado");
+        if (LocalDateTime.now().isAfter(encuentro.getHorario())) {
+            encuentro.cambiarEstado(new EnJuego(encuentro));
+            encuentro.notificar(TipoNotificacion.PARTIDO_EN_JUEGO);
+        }
+    }
+
+    @Override
+    public boolean puedeUnirse() {
+        return false;
+    }
+
+    @Override
+    public boolean puedeConfirmar() {
+        return false;
+    }
 }
--- SistemaEncuentrosJava/src/state/EnJuego.java
+++ SistemaEncuentrosJava/src/state/EnJuego.java
@@ -2,10 +2,29 @@ package state;
 
 import entities.Encuentro;
 
+package state;
+
+import entities.Encuentro;
+
 public class EnJuego implements EstadoPartido {
     private Encuentro encuentro;
-    public EnJuego(Encuentro e) { this.encuentro = e; }
-    @Override public void manejarCambioEstado() {}
-    @Override public boolean puedeUnirse() { return false; }
-    @Override public boolean puedeConfirmar() { return false; }
+
+    public EnJuego(Encuentro encuentro) {
+        this.encuentro = encuentro;
+    }
+
+    @Override
+    public void manejarCambioEstado() {
+        System.out.println("Estado actual: En Juego");
+    }
+
+    @Override
+    public boolean puedeUnirse() {
+        return false;
+    }
+
+    @Override
+    public boolean puedeConfirmar() {
+        return false;
+    }
 }
